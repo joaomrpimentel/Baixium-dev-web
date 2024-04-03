ee23801e2bfbde58259300ae228e7eedcea3a966
ViniciusgCaetano <viny6888@gmail.com>
2024-04-02 21:32:47 -0300

    feat(client): adding deletion to app

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -5,36 +5,71 @@
       :rows="posts"
       :columns="columns"
       row-key="id"
-    />
+    >
+
+        <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+                <q-btn
+                    align="center"
+                    icon="delete"
+                    @click="handleDelete(props.row.id)"
+                    color="negative"
+                />
+            </q-td>
+        </template>
+    </q-table>
   </q-page>
 </template>
 
 <script>
-
+import postsService from 'src/services/posts';
 import { defineComponent, ref, onMounted } from 'vue';
-import { api } from 'boot/axios';
+import { useQuasar } from 'quasar';
+
 export default defineComponent({
     name: 'IndexPage',
     setup () {
         const posts = ref([]);
+        const { list, post, update, remove } = postsService();
         const columns = [
             { name: 'id', field: 'id', label: 'id' },
-            { name: 'author', field: 'author', label: 'author' }
+            { name: 'author', field: 'author', label: 'author' },
+            { name: 'actions', label: 'Actions', align: 'center'}
         ]
+
+        const $q = useQuasar()
+
         onMounted(() => {
             getPosts()
         })
 
         const getPosts = async () => {
             try {
-                const { data } = await api.get('posts');
+                const data = await list();
                 console.log(data)
                 posts.value = data
             } catch (error) {
                 console.error(error);
             }
         }
-        return { posts, columns }
+
+        const handleDelete = async (id) => {
+        try {
+            $q.dialog({
+            title: 'Deletar?',
+            message: 'Deseja deletar esse post?',
+            cancel: true,
+            persistent: true
+            }).onOk(async () => {
+            await remove(id)
+            $q.notify({message: 'Apagado com sucesso', icon: 'check', color: 'positive'})
+            await getPosts()
+            })
+        } catch (error) {
+            $q.notify({message: 'Erro ao apagar', icon: 'times', color: 'negative'})
+        }
+        }
+        return { posts, columns, handleDelete }
     }
 }
 );
---
+++ client/src/services/posts.ts
@@ -0,0 +1,12 @@
+import useApi from '../composables/UseApi';
+
+export default function postsService (){
+    const{ list , post, update, remove } = useApi('posts');
+
+    return{
+        list,
+        post,
+        update, 
+        remove
+    }
+}
