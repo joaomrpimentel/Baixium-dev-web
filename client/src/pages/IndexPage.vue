a6c5d6b120a7426879b00caa3ba98507f27142de
Thalison Vinicius <69252647+Thalison17@users.noreply.github.com>
2024-04-12 21:04:38 -0300

    Update IndexPage.vue

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -2,10 +2,12 @@
     <q-header class="bg-grey-1 text-black" style="padding: 10px">
         <q-toolbar>
           <q-toolbar-title>
-            <q-avatar square>
-              <img src="https://cdn.discordapp.com/attachments/1227583527114248253/1227583583183573082/Baixium-logo.svg.svg?ex=6628ef4d&is=66167a4d&hm=d7cf8913c78feb2f1516fc8eb245cbda1355ae6715e4712fd9f9e8822c98b3f9&">
-            </q-avatar>
-            Baixium
+            <div @click="navigateTo('home')" style="width: 100px; height: 50px;">
+                <q-avatar square>
+                    <img src="https://cdn.discordapp.com/attachments/1227583527114248253/1227583583183573082/Baixium-logo.svg.svg?ex=6628ef4d&is=66167a4d&hm=d7cf8913c78feb2f1516fc8eb245cbda1355ae6715e4712fd9f9e8822c98b3f9&" @click="navigateTo('home')">
+                </q-avatar>
+                Baixium 
+          </div>
           </q-toolbar-title>
           <div style="width: 13%" class="row justify-between">
             <div>
