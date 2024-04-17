837c46d5360aeff7f881cee433f929716b45cd6c
Thalison Vinicius <69252647+Thalison17@users.noreply.github.com>
2024-04-16 21:36:46 -0300

    Update RegisterPage.vue

--- client/src/pages/RegisterPage.vue
+++ client/src/pages/RegisterPage.vue
@@ -34,7 +34,7 @@
     </q-body>
     <q-footer bordered class="bg-grey-1 text-primary">
     <q-tabs no-caps active-color="primary" indicator-color="transparent" class="text-grey-8" v-model="tab">
-        <q-tab name="About Us" label="About Us" />
+        <q-tab name="About Us" label="About Us" @click="navigateTo('aboutUs')" />
         <q-tab name="Contact" label="Contact" />
         <q-tab name="Terms & Conditions" label="Terms & Conditions" />
     </q-tabs>
