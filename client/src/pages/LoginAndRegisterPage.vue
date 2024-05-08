f05c3a11432a3c878c4cea5a57203e0c8eb85f9a
ViniciusgCaetano <viny6888@gmail.com>
2024-05-08 07:58:59 -0300

    chore(pages): removing unused code on login and register pages

--- client/src/pages/LoginAndRegisterPage.vue
+++ client/src/pages/LoginAndRegisterPage.vue
@@ -70,9 +70,8 @@
 <script>
 import MainHeader from 'src/components/MainHeader.vue';
 import MainFooter from 'src/components/MainFooter.vue';
-import postsService from 'src/services/posts';
-import { defineComponent, ref, onMounted } from 'vue';
-import { useQuasar } from 'quasar';
+import { defineComponent } from 'vue';
+
 
 export default defineComponent({
   name: 'LoginPage',
@@ -176,50 +175,7 @@ export default defineComponent({
     navigateTo(routeName) {
       this.$router.push({ name: routeName })
     }
-    },
-    setup () {
-      const posts = ref([]);
-      const { list, remove } = postsService();
-      const columns = [
-          { name: 'id', field: 'id', label: 'id' },
-          { name: 'author', field: 'author', label: 'author' },
-          { name: 'actions', label: 'Actions', align: 'center'}
-      ]
-
-      const $q = useQuasar()
-
-      onMounted(() => {
-          getPosts()
-      })
-
-      const getPosts = async () => {
-          try {
-              const data = await list();
-              console.log(data)
-              posts.value = data
-          } catch (error) {
-              console.error(error);
-          }
-      }
-
-      const handleDelete = async (id) => {
-      try {
-          $q.dialog({
-          title: 'Deletar?',
-          message: 'Deseja deletar esse post?',
-          cancel: true,
-          persistent: true
-          }).onOk(async () => {
-          await remove(id)
-          $q.notify({message: 'Apagado com sucesso', icon: 'check', color: 'positive'})
-          await getPosts()
-          })
-      } catch (error) {
-          $q.notify({message: 'Erro ao apagar', icon: 'times', color: 'negative'})
-      }
-      }
-      return { posts, columns, handleDelete }
-  },
+    }
   
 }
 );
